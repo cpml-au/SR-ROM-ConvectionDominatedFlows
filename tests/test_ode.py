d717bd564788594c703edf0109a448f7f4261e8b
Simone Manti <manti998@gmail.com>
2025-08-22 16:10:56 +0200

    General improvements to all the scripts.

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -1,16 +1,17 @@
 import numpy as np
 from sr_rom.vmsrom_solver import solve_ode
+import os
 
 
 def test_ode():
     # path to the NekROM operators
-    sdir = f"/home/smanti/SR-ROM/src/sr_rom/data/2dcyl/Re/Re400"
+    main_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    sdir = f"{main_path}/src/sr_rom/data/2dcyl/Re/Re400"
     # path to results
-    time_extrapolation_dir = "/home/smanti/SR-ROM/src/sr_rom/results_time_extrapolation/"
-    results_dir = f"{time_extrapolation_dir}2dcyl/r_2/"
-    lr_directory = results_dir + "lr/Re400/"
-    sr_directory = results_dir + "sr/Re400/"
-    nn_directory = results_dir + "nn/Re400/"
+    results_dir = f"{main_path}/tests/data/"
+    lr_directory = results_dir + "lr/"
+    sr_directory = results_dir + "sr/"
+    nn_directory = results_dir + "nn/"
 
     # load actual ucoefs
     u_lr_true = np.load(f"{lr_directory}ucoefs_lr.npy")
@@ -22,7 +23,7 @@ def test_ode():
     ene_sr_true = np.load(f"{sr_directory}energy_sr_0.npy")
     ene_nn_true = np.load(f"{nn_directory}energy_nn_0.npy")
 
-    u_lr, ene_lr = solve_ode(400, "LR", sdir, lr_directory,  100, 0.01, 1, 2, None)
+    u_lr, ene_lr = solve_ode(400, "LR", sdir, lr_directory, 100, 0.01, 1, 2, None)
     u_sr, ene_sr = solve_ode(400, "SR", sdir, sr_directory, 100, 0.01, 1, 2, None)
     u_nn, ene_nn = solve_ode(400, "NN", sdir, nn_directory, 100, 0.01, 1, 2, None)
 
